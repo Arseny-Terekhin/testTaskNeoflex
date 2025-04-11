310e6ce147c7ccb37ac25664c98b017041ce0071
Arseny <terexin-2003@mail.ru>
2025-04-11 14:48:11 +0300

    update service

--- src/main/java/com/example/calcSpringNeoFlex/service/VacationPayCountService.java
+++ src/main/java/com/example/calcSpringNeoFlex/service/VacationPayCountService.java
@@ -1,23 +1,53 @@
 package com.example.calcSpringNeoFlex.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class VacationPayCountService {
 
-    public long calculateWorkDays(LocalDate startDate, int countVacationDays){
-        long workDays = 0;
+    @Value("${vacation.month.avg.days}")
+    double avgMonthDays;
+
+    private List<LocalDate> holidays ;
+
+    @Autowired
+    private void setHolidays(@Value("${list.holidays}") String stringListHolidays){
+        this.holidays = Arrays.stream(stringListHolidays.split(","))
+                .map(LocalDate :: parse)
+                .collect(Collectors.toList());
+    }
+
+    public int calculateWorkDays(LocalDate startDate, int countVacationDays){
+        int workDays = 0;
         LocalDate date = startDate;
 
         for (int i = 0; i <  countVacationDays; i++) {
-            if (date.getDayOfWeek() != DayOfWeek.SATURDAY && date.getDayOfWeek() != DayOfWeek.SUNDAY) {
+            if (isWorkdays(date)) {
                 workDays++;
             }
             date = date.plusDays(1);
         }
         return workDays;
+    }
+
+    public double calculateVacationPay(double amountOfSalary, int countDays, LocalDate startDate) {
+        if (startDate != null) {
+            int workDays = calculateWorkDays(startDate, countDays);
+            return amountOfSalary / avgMonthDays * workDays;
+        } else {
+            return amountOfSalary / avgMonthDays * countDays;
+        }
+    }
+
+    private boolean isWorkdays(LocalDate date){
+        return !(date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY || holidays.contains(date));
     }
 }
--- src/test/java/com/example/calcSpringNeoFlex/CalcSpringNeoFlexApplicationTests.java
+++ src/test/java/com/example/calcSpringNeoFlex/CalcSpringNeoFlexApplicationTests.java
@@ -1,13 +1,81 @@
 package com.example.calcSpringNeoFlex;
 
+import com.example.calcSpringNeoFlex.service.VacationPayCountService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CalcSpringNeoFlexApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private VacationPayCountService vacationPayCountService;
+
+    @Test
+        //тест на вычисление рабочих дней, когда отпуск задевает все праздничные дни 1 января - 8 января
+    void testCalculateWorksDays_Test1() {
+        LocalDate startDate = LocalDate.of(2025, 1, 1);
+        int vacationsDays = 5;
+
+        int expectedWorkDays = 0;
+        int actualWorkDays = vacationPayCountService.calculateWorkDays(startDate, vacationsDays);
+
+        assertEquals(expectedWorkDays, actualWorkDays);
+    }
+
+    @Test
+        //тест когда отпуск затрагивает несколько праздничных/выходных дней
+    void testCalculateWorksDays_Test2() {
+        LocalDate startDate = LocalDate.of(2025, 1, 8);
+        int vacationsDays = 7;
+
+        int expectedWorkDays = 4;
+        int actualWorkDays = vacationPayCountService.calculateWorkDays(startDate, vacationsDays);
+
+        assertEquals(expectedWorkDays, actualWorkDays);
+    }
+
+    @Test
+        //Расчет отпускных, когда 0 рабочих дней
+    void testCalculateVacationPay_Test1() {
+        LocalDate startDate = LocalDate.of(2025, 1, 1);
+        int vacationsDays = 5;
+        double amountOfSalary = 150000;
+
+        double expectedVacationPay = 0;
+        double actualVacationPay = vacationPayCountService.calculateVacationPay(amountOfSalary, vacationsDays, startDate);
+
+        assertEquals(expectedVacationPay, actualVacationPay);
+    }
+
+    @Test
+        //Расчет отпускных, когда 4 рабочих дня
+    void testCalculateVacationPay_Test2() {
+        LocalDate startDate = LocalDate.of(2025, 1, 8);
+        int vacationsDays = 7;
+        double amountOfSalary = 150000;
+
+        double expectedVacationPay = 20477.8;
+        double actualVacationPay = vacationPayCountService.calculateVacationPay(amountOfSalary, vacationsDays, startDate);
+
+        assertEquals(expectedVacationPay, actualVacationPay, 0.1);
+    }
+
+    @Test
+        //Расчет отпускных, когда не указанна начальная дата при 5 дневном отпуске
+    void testCalculateVacationPay_Test3() {
+        LocalDate startDate = null;
+        int vacationsDays = 5;
+        double amountOfSalary = 150000;
+
+        double expectedVacationPay = 25597.2;
+        double actualVacationPay = vacationPayCountService.calculateVacationPay(amountOfSalary, vacationsDays, startDate);
+
+        assertEquals(expectedVacationPay, actualVacationPay, 0.1);
+    }
 
 }
